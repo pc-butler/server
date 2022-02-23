2e9e01a6c2fab2240329a22df5670dfef069c106
Royce <roycewhitaker@rocketmail.com>
2022-02-23 12:15:16 -0800

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -221,6 +221,8 @@ Coded by www.creative-tim.com
   <script src="./assets/js/plugins/chartjs.min.js"></script>
   <!--  Notifications Plugin    -->
   <script src="./assets/js/plugins/bootstrap-notify.js"></script>
+   
+   <script src="./assets/js/demo.js"></script>
   <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="./assets/js/paper-dashboard.min.js?v=2.0.1" type="text/javascript"></script>
 </body>
